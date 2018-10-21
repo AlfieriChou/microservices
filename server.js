5669e6093c0d80aa18e194da49624cd917bed86b
AlfieriChou <alfierichou@gmail.com>
2018-10-21 13:20:29 +0800

    add notfound

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ const hello = async (req, res) => {
   micro.send(res, 200, await Promise.resolve(`Hello ${req.params.who}`))
 }
 
-module.exports = router(get('/hello/:who', hello))
+const notfound = (req, res) => {
+  micro.send(res, 404, 'Not found route')
+}
+
+module.exports = router(get('/hello/:who', hello), get('/*', notfound))
