cb3c0d60f16102fdc00ace65a4e1d77927d07587
AlfieriChou <alfierichou@gmail.com>
2018-10-21 14:36:04 +0800

    add morgan to handle req/res logger

--- app/hello.js
+++ app/hello.js
@@ -1,6 +1,10 @@
 const micro = require('micro')
+const pify = require('pify')
+const morgan = require('morgan')
+const logger = pify(morgan('combined'))
 
 const hello = async (req, res) => {
+  await logger(req, res)
   micro.send(res, 200, await Promise.resolve(`Hello ${req.params.who}`))
 }
 
--- app/notfound.js
+++ app/notfound.js
@@ -1,6 +1,10 @@
 const micro = require('micro')
+const pify = require('pify')
+const morgan = require('morgan')
+const logger = pify(morgan('combined'))
 
-const notfound = (req, res) => {
+const notfound = async (req, res) => {
+  await logger(req, res)
   micro.send(res, 404, 'Not found route')
 }
 
