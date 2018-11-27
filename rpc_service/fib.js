ea4ad16765d6bf692dcb09307ee86f0566fb03b1
AlfieriChou <alfierichou@gmail.com>
2018-11-27 20:32:25 +0800

    remove event-stream and flatmap-stream

--- rpc_service/fib.js
+++ rpc_service/fib.js
@@ -2,15 +2,15 @@ const config = require('../config/config')
 const jackrabbit = require('jackrabbit')
 const rabbit = jackrabbit(config.url)
 const exchange = rabbit.default()
-const rpc = exchange.queue({ name: "rpc_queue", prefetch: 1, durable: false })
+const rpc = exchange.queue({ name: 'rpc_queue', prefetch: 1, durable: false })
 rpc.consume(onRequest)
 
-function onRequest(data, reply) {
-  console.log("got request for n:", data.n)
+function onRequest (data, reply) {
+  console.log('got request for n:', data.n)
   reply({ result: fib(data.n) })
 }
 
-function fib(n) {
+function fib (n) {
   if (n === 0) return 0
   if (n === 1) return 1
   return fib(n - 1) + fib(n - 2)
--- work_queue/english.js
+++ work_queue/english.js
@@ -4,7 +4,7 @@ const rabbit = jackrabbit(config.url)
 const exchange = rabbit.default()
 const hello = exchange.queue({ name: 'task_queue', durable: true })
 hello.consume(onGreet)
-function onGreet(data, ack) {
+function onGreet (data, ack) {
   console.log('Hello, ' + data.name + '!')
   ack()
 }
